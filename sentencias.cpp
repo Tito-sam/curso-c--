b1d2bb210626844879047d4bc5a826bc11c8f665
Tito Samuel Mendez Orjuela <totitron20@gmail.com>
2023-12-06 08:57:16 -0500

    inicializacion github

---
+++ sentencias.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() {
+
+	/*
+		Bucles
+		Son la base de practicamente todos los lenguajes de programacion,
+		ya que permiten realizar muchos tipos de tareas y problemas repetitivos,
+		y gracias a esto, se utilizan en muchos tipos de problemas. 
+
+		"While"
+
+		Este es el bucle mas sencillo, aunque se utiliza mucho, por su potencia.
+		Su sintaxis es la siguiente:
+		while (condicion) {
+			<sentencia>
+		}
+		consiste en ejecutar una sentencia u bloque de codigo, mientras una condicion
+		se cumple, y por esta funcion, se logran muchas resoluciones de problemas.
+
+		"do while"
+
+		Es Practicamente el mismo que el anterior, pero primero ejecuta la sentencia y luego
+		evalua la condicion, por lo que por lo menos entrara una vez al bloque de codigo.
+		Su sintaxis es la siguiente:
+		do {
+			<sentencia>
+		} while (condicion);
+		
+		"for"
+		es el tipo de bucle mas elaborado, se establece desde el inicio cuanto va a durar y como 
+		van a ser los pasos.
+		La sintaxis es la siguiente:
+		for (variable inicial; condicion; incremento) {
+			<sentencia>
+		}
+
+		
+	*/
+    
+}
